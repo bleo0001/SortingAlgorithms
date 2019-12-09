0b05adda1ba91c424a040c48a7b1e062930490d0
bleo0001 <30591622+bleo0001@users.noreply.github.com>
2019-12-09 14:40:45 +1100

    Bubble Sort working

--- script.js
+++ script.js
@@ -35,6 +35,36 @@ for (var i = 0;i<Values.length;i++){
 }
 }
 
+
+function SortingGraph(highlighted1, highlighted2) { //draws the barchart onto the canvas
+    var myCanvas = document.getElementById("myCanvas");
+    myCanvas.width = 1200;
+    myCanvas.height = 500;
+    var ctx = myCanvas.getContext("2d");
+    
+    
+    var width = 500/Values.length; //Rectangle width
+    var currX = 1; //space between Rectangles
+    var base = 400; //Canvas Height
+    
+    
+    
+    for (var i = 0;i<Values.length;i++){
+        if (i == highlighted1){
+            ctx.fillStyle = 'yellow';
+        } else if(i == highlighted2) {
+            ctx.fillStyle = 'Orange';
+        } else {
+            ctx.fillStyle = 'green' ;
+        }
+        var h = Values[i];
+        ctx.fillRect(currX,myCanvas.height-h,width,h);
+        currX += width + 10;
+    }
+    }
+
+
+
 function GenerateArray() {  //Clears and repopulates the array with random values
     Values = [];
     for (var i=0; i<ArraySize; i++) {
@@ -43,16 +73,22 @@ function GenerateArray() {  //Clears and repopulates the array with random value
     DrawGraph();
 }
 
-function bubbleSort(){
+ async function bubbleSort(){
     var len = Values.length;
-    for (var i = len-1; i>=0; i--){
+    for (var i = len-1; i>=0; i--){  
       for(var j = 1; j<=i; j++){
         if(Values[j-1]>Values[j]){
+            SortingGraph(j-1,j);
+            await sleep(200);
             var temp = Values[j-1];
-            Values[j-1] = Values[j];
+         Values[j-1] = Values[j],50000
             Values[j] = temp;
          }
       }
     }
-   DrawGraph();
+    setTimeout(DrawGraph(),999999999);
  }
+
+ function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+  }
